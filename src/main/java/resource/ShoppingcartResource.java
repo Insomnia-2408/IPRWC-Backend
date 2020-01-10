fafe6e0676558639f8005241055e4c5d72e57f9a
Valerie <valerietimmerman2000@gmail.com>
2020-01-10 15:34:50 +0100

    Fixed error when starting server.

--- src/main/java/resource/ShoppingcartResource.java
+++ src/main/java/resource/ShoppingcartResource.java
@@ -8,8 +8,6 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.security.NoSuchAlgorithmException;
-import java.util.List;
 
 @Path("/shoppingcart")
 public class ShoppingcartResource {
